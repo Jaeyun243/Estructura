3e6d491e7600e1854a58c464c8b43e856cddf61e
Jaeyun243 <ricardo.pazmino@epn.edu.ec>
2023-07-16 22:11:15 -0500

    Subir proyecto estructura

---
+++ OperacionClase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseEstructura
+{
+    internal class OperacionClase
+    {
+        public int num1 { get; set; }
+        public int num2 { get; set; }  
+
+        public OperacionClase(int x, int y)
+        {
+            this.num1 = x;
+            this.num2 = y; 
+        }
+        public void incremento(OperacionClase e, int aumento)
+        {
+            e.num1 += aumento;  
+            e.num2 += aumento;  
+        }
+
+        public override string ToString()
+        {
+            return string.Format("El valor de numero 1 y numero 2 son {0},{1}",this.num1,this.num2);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ClaseEstructura
+{
+    internal class Program
+    {
+        //Estructura cambio por tipo de valor sus valores iniciales no se modifican
+        //Clase cambio por tipo de rerefencia sus valores iniciales se modifican
+        static void Main(string[] args)
+        {
+            OperacionClase o1 = new OperacionClase(4,5);
+            OperacionEstructura o2 = new OperacionEstructura(4, 5);
+            //Valores que se tiene al inicio 
+            Console.WriteLine("Valores iniciales de la clase operacion");
+            Console.WriteLine(o1.ToString());
+            Console.WriteLine("Valores iniciales de la estructura operacion");
+            Console.WriteLine(o2.ToString());
+            //Se llama al metodo incremento
+            o1.incremento(o1, 10);
+            o2.incremento(o2, 10);
+            // Imprimimos valores finales ahora que se llamo al metod incremento
+            Console.WriteLine("Valores Finales de la clase operacion");
+            Console.WriteLine(o1.ToString());
+            Console.WriteLine("Valores Finales de la estructura operacion");
+            Console.WriteLine(o2.ToString());
+
+        }
+    }
+}
